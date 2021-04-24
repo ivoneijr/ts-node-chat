18fe85e932a2ecbe5339b96b091fe67d980a3cce
ivonei dos santos marques junior <ivonei.junior@iwsbrazil.com>
2021-04-24 19:39:55 -0300

    refactor

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -3,8 +3,14 @@ import cron from 'node-cron'
 
 import { CronsService } from '../services/crons'
 import sample from './sample'
+import { Cron } from '../entities/Cron'
 
-const getWorkers = async () => {
+interface IWorkers {
+  cron?: Cron
+  run?(): void
+}
+
+const getWorkers = async (): Promise<IWorkers[]> => {
   const cronsService = new CronsService()
 
   return [
@@ -20,10 +26,16 @@ const getWorkers = async () => {
 }
 
 const run = async () => {
-  const success = await createConnection()
-  const workers = success ? await getWorkers() : []
+  try {
+    const success = await createConnection()
+    const workers = success ? await getWorkers() : []
 
-  workers.map(worker => cron.schedule(worker.cron.value, worker.run))
+    workers.map(worker =>
+      worker.cron ? cron.schedule(worker.cron.value, worker.run) : null
+    )
+  } catch (error) {
+    console.log(`[error] running workers ${error}`)
+  }
 }
 
 run()
